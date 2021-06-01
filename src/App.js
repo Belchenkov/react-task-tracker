e71dfaa8ccee5911b6803e67593e2aac99da0954
Aleksey Belchenkov <u608110@gmail.com>
2021-06-01 16:19:23 +0400

    Add new Task

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import Header from "./components/Header";
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
     const [tasks, setTasks] = useState([
@@ -19,6 +20,16 @@ function App() {
         }
     ]);
 
+    const addTask = task => {
+        const id = Math.floor(Math.random() * 1000) + 1;
+        const newTask = {
+            id,
+            ...task
+        };
+
+        setTasks([...tasks, newTask]);
+    };
+
     const deleteTask = id => {
         setTasks(tasks.filter(task => task.id !== id));
     };
@@ -30,6 +41,8 @@ function App() {
     return (
         <div className="container">
             <Header title="Task Tracker" />
+            <AddTask onAddTask={addTask} />
+
             {
                 tasks.length > 0 ? (
                     <Tasks
