1320cdd5c7f140e701ebabef5809389c84207d28
Aleksey Belchenkov <u608110@gmail.com>
2021-06-01 15:38:27 +0400

    Create Tasks, Task Components & Delete Task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
+import { useState } from 'react';
+
 import Header from "./components/Header";
+import Tasks from './components/Tasks';
 
 function App() {
-  return (
-    <div className="container">
-      <Header title="Task Tracker" />
-    </div>
+    const [tasks, setTasks] = useState([
+        {
+            "id": 1,
+            "text": "Doctors Appointment",
+            "day": "Feb 5th at 2:30pm",
+            "reminder": true
+        },
+        {
+            "id": 2,
+            "text": "Meeting at School",
+            "day": "Feb 6th at 1:30pm",
+            "reminder": true
+        }
+    ]);
+
+    const deleteTask = id => {
+        setTasks(tasks.filter(task => task.id !== id));
+    };
+
+    return (
+        <div className="container">
+            <Header title="Task Tracker" />
+            {
+                tasks.length > 0 ? (
+                    <Tasks
+                        onDelete={deleteTask}
+                        tasks={tasks}
+                    />
+                ) : (
+                    'No Tasks To Show'
+                )
+            }
+
+
+        </div>
   );
 }
 
